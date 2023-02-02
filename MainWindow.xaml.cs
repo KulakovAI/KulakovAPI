e2cc29bffb127bbe35868f987f3dc88351450fdc
godiw <godiw@DESKTOP-KAA4FKO>
2023-02-02 21:52:27 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Newtonsoft.Json;
+using System.IO;
+using System.Net;
+
+namespace KulakovAPI
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BOX_Loaded(object sender, RoutedEventArgs e)
+        {
+            string url = "https://api.openweathermap.org/data/3.0/onecall?lat=33.44&lon=-94.04&exclude=hourly,daily&appid=7507b58567080e9dd771cb190d1c3020";
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(url);
+            HttpWebResponse res = (HttpWebResponse)req.GetResponse();
+            StreamReader reader = new StreamReader(res.GetResponseStream());
+            string response = reader.ReadToEnd();
+            BOX.Text = response;
+            WeatherResponser wr = JsonConvert.DeserializeObject<WeatherResponser>(response);
+            City.Text = wr.Name;
+            Temperature.Text = wr.Main.Temp.ToString();
+        }
+    }
+}
---
+++ WeatherResponser.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KulakovAPI
+{
+    internal class WeatherResponser
+    {
+        public string Name { get; set; }
+        public MainInfo Main { get; set; }
+    }
+
+    class MainInfo
+    {
+        public string Temp { get; set; }
+    }
+}
